aac07faefb6b7d5d8eff9beebd8feddb57bc1a41
Guillaume Laville <guillaume.laville@etu.univ-lyon1.fr>
2022-07-28 20:58:26 +0200

    hero added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>Hello world</h1>
+  <div>
+    <HeroApp />
+  </div>
 </template>
 
 <script>
+import HeroApp from "@/components/heroApp/heroApp.vue";
+
+export default {
+  components: {
+    HeroApp,
+  },
+};
 </script>
 
-<style lang="scss" src="./App.scss">
+<style lang="scss">
+@font-face {
+  font-family: OpenDyslexic;
+  src: url("./assets/fonts/OpenDyslexic-Regular.otf");
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  position: relative;
+  text-align: center;
+  font-family: OpenDyslexic;
+}
 </style>
---
+++ src/components/heroApp/heroApp.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hero">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroApp",
+};
+</script>
+
+<style lang="scss">
+$green: #05ab60;
+$greenDark: #1f5b1d;
+
+.hero {
+  width: 100%;
+  height: 100vh;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: $green;
+  background: linear-gradient(-45deg, rgba(5,171,96,1) 0%, rgba(31,91,29,1) 20%, rgba(10,94,83,1) 40%, rgba(14,158,139,1) 60%, rgba(14,136,158,1) 80%, rgba(4,119,198,1) 100%);
+  background-size: 600% 600%;
+
+  animation: gradientBg 20s infinite ease-in-out;
+}
+
+@keyframes gradientBg {
+  
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 100%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+</style>
